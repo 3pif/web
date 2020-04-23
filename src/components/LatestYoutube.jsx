d4426fb044025144bdaa8e3202b107e1d4c131f6
Robinhuett <5955614+Robinhuett@users.noreply.github.com>
2020-04-23 19:38:36 +0200

    style: Use round corners for yt embed

--- src/components/LatestYoutube.jsx
+++ src/components/LatestYoutube.jsx
@@ -6,7 +6,7 @@ const Wrapper = styled.div`
   width: 100%;
   height: 0;
   padding-bottom: 56.25%;
-`;
+  `;
 
 const Video = styled.iframe`
   position: absolute;
@@ -15,6 +15,10 @@ const Video = styled.iframe`
   width: 100%;
   height: 100%;
   box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 6px 10px 0px rgba(0, 0, 0, 0.14), 0px 1px 18px 0px rgba(0, 0, 0, 0.12);
+
+  @media (min-width: 650px) {
+    border-radius: 5px;
+  }
 `;
 
 class LatestYoutube extends React.Component {
